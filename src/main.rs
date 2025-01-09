b894c598243f8a3757c5db65672f329574d3ee50
younghyun-gn <younghyun.chi@genesisnest.com>
2025-01-09 17:13:28 +0900

    feat: basic funcs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use aws_sdk_sqs::Client;
+
+struct SQSMessage {
+    body: String,
+}
+
+#[tokio::main(flavor = "multi_thread")]
+async fn main() {
+    tracing_subscriber::fmt::init();
+}
+
+async fn send(client: &Client, queue_url: &str, message: &SQSMessage) -> anyhow::Result<()> {
+    let rsp = client
+        .send_message()
+        .queue_url(queue_url)
+        .message_body(&message.body)
+        // If the queue is FIFO, you need to set .message_deduplication_id
+        // and message_group_id or configure the queue for ContentBasedDeduplication.
+        .send()
+        .await?;
+
+    Ok(())
+}
+
+async fn receive(client: &Client, queue_url: &String) -> Result<(), Error> {
+    let rcv_message_output = client.receive_message().queue_url(queue_url).send().await?;
+
+    // ten at a time
+    for message in rcv_message_output.messages.unwrap_or_default() {
+        println!("Got the message: {:#?}", message);
+    }
+
+    Ok(())
 }
